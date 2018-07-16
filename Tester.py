cb5abd1c708632e9e89085a1696a27364e734e70
WhiteCat <40523532+cjong93@users.noreply.github.com>
2018-07-15 21:42:52 -0400

    Initial commit

---
+++ Tester.py
@@ -0,0 +1,12 @@
+class Tester:
+    def __init__(self):
+        self.value = 0
+        self.base = 0
+
+
+if __name__ == '__main__':
+    tester = Tester()
+    print("tester is running")
+    print(f"tester value is {tester.value}")
+    print(f"tester base is {tester.base}")
+    print("test")
